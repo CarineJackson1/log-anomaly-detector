dbad57d62344e9933a2446c985712f4d5625c6e2
Key'n Brosdahl <keynb1999@gmail.com>
2025-08-06 17:30:59 -0500

    HomePage-Keyn

--- frontend/src/components/nav/Navbar.tsx
+++ frontend/src/components/nav/Navbar.tsx
@@ -1,40 +1,39 @@
 import { Link } from "react-router-dom";
+import './NavStyles.css'
 
 function Navbar() {
   return (
-    <nav className="navbar navbar-light bg-light shadow-sm px-5">
-      <div className="d-flex justify-content-between w-100 align-items-center">
-        <Link className="navbar-brand fw-bold" to="/">
+    <nav className="navbar">
+        <Link className="navbar-brand" to="/">
           AstroSkill
         </Link>
-        <ul className="navbar-nav d-flex flex-row mb-0">
-          <li className="nav-item mx-2">
+        <ul className="NavLink-Container">
+          <li className="NavLinks">
             <Link className="nav-link" to="/#mission">
               Mission
             </Link>
           </li>
-          <li className="nav-item mx-2">
+          <li className="NavLinks">
             <Link className="nav-link" to="/login">
               Login
             </Link>
           </li>
-          <li className="nav-item mx-2">
+          <li className="NavLinks">
             <Link className="nav-link" to="/register">
               Register
             </Link>
           </li>
-          <li className="nav-item mx-2">
+          <li className="NavLinks">
             <Link className="nav-link" to="/employer">
               Employer
             </Link>
           </li>
-          <li className="nav-item mx-2">
+          <li className="NavLinks">
             <Link className="nav-link" to="/learner">
               Learner
             </Link>
           </li>
         </ul>
-      </div>
     </nav>
   );
 }
